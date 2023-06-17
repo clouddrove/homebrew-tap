7d9937839f9ee28467721e1a6e8a082061577f55
Anmol Nagpal <ianmolnagpal@gmail.com>
2023-06-17 16:20:17 -0400

    feat: update version

--- kuconf.rb
+++ kuconf.rb
@@ -2,29 +2,29 @@ class Kuconf < Formula
   desc "Update kubeconfig for all clusters reachable from all AWS profiles"
   homepage "https://github.com/clouddrove/kuconf"
   license "Apache-2.0"
-  version "v0.0.1"
+  version "v0.0.3"
 
   on_macos do
 
     if Hardware::CPU.intel?
-      url "https://github.com/clouddrove/kuconf/releases/download/v0.0.1/kuconf-darwin-amd64.zip"
-      sha256 "541e377b084e908f5b30d3d97f1a0d9c55c5b55f33ff53f899d015f459a909f4"
+      url "https://github.com/clouddrove/kuconf/releases/download/v0.0.3/kuconf-darwin-amd64.zip"
+      sha256 "36372e09b01bcba5d7d8884d0cda921f26470fbd6a4ed8b0b428d9f561f9fb91"
     end
 
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/clouddrove/kuconf/releases/download/v0.0.1/kuconf-darwin-arm64.zip"
-      sha256 "2f467f0e98255f525e1a1cd8d9b1f704cf19a3c194b1da009b960e521a615671"
+      url "https://github.com/clouddrove/kuconf/releases/download/v0.0.3/kuconf-darwin-arm64.zip"
+      sha256 "e979e88b42acf3c5b6a30c5a5833198ca36da55644dccdcda8928c9c335adf21"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/clouddrove/kuconf/releases/download/v0.0.1/kuconf-linux-amd64.zip"
-      sha256 "d3fc57921e6ba3c2fd8efdaa5e54e7f5a042ad0a638cda1e69f0bc5bbde18cc0"
+      url "https://github.com/clouddrove/kuconf/releases/download/v0.0.3/kuconf-linux-amd64.zip"
+      sha256 "cd848d25a97faebd6f5cf1c2ab6b174affb8d3b30847320896bbc9d8b1ccf947"
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/clouddrove/kuconf/releases/downloa/v0.0.1/kuconf-linux-arm64.zip"
-      sha256 "966a65422eab637f84fc8ae923d112a7b0c1e81f26369f97c5062d96ca8c68a4"
+      url "https://github.com/clouddrove/kuconf/releases/download/v0.0.3/kuconf-linux-arm64.zip"
+      sha256 "10248568404f5ae2fb9833e6172514a2f6ea8f211c34fb228db3e1f7816a0915"
     end
   end
 
