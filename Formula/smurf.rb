0004a59d35c244a5c4c22f2c52b291340f513ada
Anket Satbhai <anket.satbhai@clouddrove.com>
2026-01-12 18:42:34 +0530

    fix/ update formula for debug

--- Formula/smurf.rb
+++ Formula/smurf.rb
@@ -7,7 +7,7 @@ class Smurf < Formula
   on_macos do
     on_arm do
       url "https://github.com/clouddrove/smurf/releases/download/v0.1.0/smurf-v0.1.0-darwin-arm64.tar.gz"
-      sha256 "c550f108b395386e3b8b96f5a39c7c2ebf7e63b4b06391a9642d5aa38f3ad70d"
+      sha256 "6079654a2a638149c900aa4ccaa36f94cc1804902a9f1fd4918e721b77f2b4b5"
     end
     
     on_intel do
