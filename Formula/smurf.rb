7af9fc43f9e3dbfde8ee37758fafd43b6efcb082
Anket Satbhai <anket.satbhai@clouddrove.com>
2026-01-21 23:22:44 +0530

    feat/update smurf new version

--- Formula/smurf.rb
+++ Formula/smurf.rb
@@ -1,30 +1,30 @@
 class Smurf < Formula
   desc "CloudNative CI/CD Management Tool"
   homepage "https://github.com/clouddrove/smurf"
-  version "0.1.0"
+  version "0.1.3"
   license "Apache-2.0"
 
   on_macos do
     on_arm do
-      url "https://github.com/clouddrove/smurf/releases/download/v0.1.0/smurf-v0.1.0-darwin-arm64.tar.gz"
-      sha256 "c550f108b395386e3b8b96f5a39c7c2ebf7e63b4b06391a9642d5aa38f3ad70d"
+      url "https://github.com/clouddrove/smurf/releases/download/v0.1.3/smurf-v0.1.3-darwin-arm64.tar.gz"
+      sha256 "003e7fc9f73915d370f34d2b1a7269563ad978f6955d73125cdf68c3eb521130"
     end
     
     on_intel do
-      url "https://github.com/clouddrove/smurf/releases/download/v0.1.0/smurf-v0.1.0-darwin-amd64.tar.gz"
-      sha256 "09ea6e3aa62d780c61bdb720026ca74689e3cdbbdc416b17d520c1719a17a8dd"
+      url "https://github.com/clouddrove/smurf/releases/download/v0.1.3/smurf-v0.1.3-darwin-amd64.tar.gz"
+      sha256 "6fe19ab95281ae6b1760646d1b5ae066ed738643fde0a6214f47cf4d4620e1da"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/clouddrove/smurf/releases/download/v0.1.0/smurf-v0.1.0-linux-amd64.tar.gz"
-      sha256 "a691a72005cc130073441a4bd9275409b54a6daa88a816db17ee999dd8d2526c"
+      url "https://github.com/clouddrove/smurf/releases/download/v0.1.3/smurf-v0.1.3-linux-amd64.tar.gz"
+      sha256 "0ada7c20917ffd24b72e6d9c8eca7af275f19c11c35e74f34c106c4a7a410752"
     end
     
     on_arm do
-      url "https://github.com/clouddrove/smurf/releases/download/v0.1.0/smurf-v0.1.0-linux-arm64.tar.gz"
-      sha256 "0c752a182218e18c7d8a0347e559c3392c5efc256637e1016f422de2aacac9a4"
+      url "https://github.com/clouddrove/smurf/releases/download/v0.1.3/smurf-v0.1.3-linux-arm64.tar.gz"
+      sha256 "0c1cc6bb36ab7b57849e16930455ace57baa89a55d29b9d8bf09eeee751c1456"
     end
   end
 
