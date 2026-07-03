32b79d05551ba0fee25582631a202b97d154a856
Anket Satbhai <anket.satbhai@clouddrove.com>
2026-07-03 18:32:45 +0530

    chore: update smurf version

--- Formula/smurf.rb
+++ Formula/smurf.rb
@@ -1,30 +1,30 @@
 class Smurf < Formula
   desc "CloudNative CI/CD Management Tool"
   homepage "https://github.com/clouddrove/smurf"
-  version "1.1.3"
+  version "1.1.5"
   license "Apache-2.0"
 
   on_macos do
     on_arm do
-      url "https://github.com/clouddrove/smurf/releases/download/v1.1.3/smurf-v1.1.3-darwin-arm64.tar.gz"
-      sha256 "40def12258f64b3890db20c4dd716d22eaee6078239b961cc4cae354f2b7c27f"
+      url "https://github.com/clouddrove/smurf/releases/download/v1.1.5/smurf-v1.1.5-darwin-arm64.tar.gz"
+      sha256 "f7f1d15e66040f8552f468bdefc5899178c936a00631d0c0283a6413c73ba895"
     end
     
     on_intel do
-      url "https://github.com/clouddrove/smurf/releases/download/v1.1.3/smurf-v1.1.3-darwin-amd64.tar.gz"
-      sha256 "b21cfdc8fa6230652e69e50d603e83899bf7194f30e9fd9b086aee80951405f9"
+      url "https://github.com/clouddrove/smurf/releases/download/v1.1.5/smurf-v1.1.5-darwin-amd64.tar.gz"
+      sha256 "d78047843921d84b3168f5186176d752b3bfe4ab59e5b4b03f012bdc4b5dd418"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/clouddrove/smurf/releases/download/v1.1.3/smurf-v1.1.3-linux-amd64.tar.gz"
-      sha256 "1b8cacb60068d904b2124f9f56c6ce0f51068a3e37d9bcf5e4617a96dfa59dd7"
+      url "https://github.com/clouddrove/smurf/releases/download/v1.1.5/smurf-v1.1.5-linux-amd64.tar.gz"
+      sha256 "524babfc035e267bd5237aef83984fa510461250626788cb976b7f91c8f4f07d"
     end
     
     on_arm do
-      url "https://github.com/clouddrove/smurf/releases/download/v1.1.3/smurf-v1.1.3-linux-arm64.tar.gz"
-      sha256 "8349627f69193762c4486b0ca1df00ea098f99d15d08d8af25275a93a456b02f"
+      url "https://github.com/clouddrove/smurf/releases/download/v1.1.5/smurf-v1.1.5-linux-arm64.tar.gz"
+      sha256 "fccd0d9290b7247805ebed1570443b2645416d68f063fda26a3ed18a15d41fbd"
     end
   end
 
